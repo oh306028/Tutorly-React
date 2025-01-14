a136bf96e92b6185164fe9cb5f5ee7a75b90579a
Oskar <hamerlao@gmail.com>
2025-01-14 21:36:47 +0100

    deleted the old home styles and replaced it with the module

---
+++ app/src/Components/Login.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <input placeholder="Email..." />
+      <input placeholder="Password..." />
+    </>
+  );
+};
+
+export default Login;
