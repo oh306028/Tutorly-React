7719edb667151d0cb6df52d1f2a54e1243998fd3
Oskar <hamerlao@gmail.com>
2025-01-15 19:17:30 +0100

    deleted the login component pop up in the same component, added redirect

--- app/src/Components/Home.jsx
+++ app/src/Components/Home.jsx
@@ -3,16 +3,12 @@ import TutorlyLogo from "../assets/images/Tutorly-logo.png";
 import styles from "../Styles/Home.module.css";
 import registerImg from "../assets/images/registerIcon.png";
 import Login from "./Login.jsx";
+import { NavLink } from "react-router-dom";
 
 export const Home = () => {
   const [isClickedAboutUs, setisClickedAboutUs] = useState(false);
   const [isClickedLearn, setisClickedLearn] = useState(false);
   const [isClickedTeach, setisClickedTeach] = useState(false);
-  const [isClickedLogin, setIsClickedLogin] = useState(false);
-
-  const handleLoginClick = () => {
-    setIsClickedLogin(!isClickedLogin);
-  };
 
   const handleTeachClick = () => {
     setisClickedTeach(!isClickedTeach);
@@ -38,70 +34,58 @@ export const Home = () => {
         </div>
 
         <div className={styles.rightMenuContainer}>
-          {(!isClickedLogin && (
-            <>
-              <Login /> <button onClick={handleLoginClick}>Back</button>
-            </>
-          )) || (
-            <>
-              <ul className={styles.menuSection}>
-                <li onClick={handleLoginClick}>LOGIN</li>
-                <hr></hr>
-                <li onClick={handleLearnClick}>
-                  LEARN
-                  {isClickedLearn && (
-                    <>
-                      <p>
-                        Register as a <b>student</b> and start your educational
-                        journey! Join our community and search for your dream
-                        tutor!
-                      </p>
-                      <a href="google.com">
-                        <img
-                          className={styles.registerImg}
-                          src={registerImg}
-                        ></img>
-                      </a>
-                    </>
-                  )}
-                </li>
-                <hr></hr>
-                <li onClick={handleTeachClick}>
-                  TEACH
-                  {isClickedTeach && (
-                    <>
-                      <p>
-                        Register as a <b>teacher</b> and start posting your
-                        educational offers! Join our tutors family and create
-                        your own bussiness!
-                      </p>
-                      <a href="google.com">
-                        <img
-                          className={styles.registerImg}
-                          src={registerImg}
-                        ></img>
-                      </a>
-                    </>
-                  )}
-                </li>
-                <hr></hr>
-                <li onClick={handleAboutUsClick}>
-                  ABOUT US
-                  {isClickedAboutUs && (
-                    <p>
-                      <b>Tutorly </b> is a web app connecting students with
-                      tutors. Students can search for tutors by subject,
-                      location, and availability. Tutors create profiles and
-                      post listings detailing their expertise, pricing, and
-                      schedules
-                    </p>
-                  )}
-                </li>
-                <hr></hr>
-                <li onClick={() => handleContactUsClick}>CONTACT US</li>
-              </ul>
-            </>
-          )}
+          <ul className={styles.menuSection}>
+            <li>
+              <NavLink className={styles.link} to="/accounts/login">
+                LOGIN
+              </NavLink>
+            </li>
+            <hr></hr>
+            <li onClick={handleLearnClick}>
+              LEARN
+              {isClickedLearn && (
+                <>
+                  <p>
+                    Register as a <b>student</b> and start your educational
+                    journey! Join our community and search for your dream tutor!
+                  </p>
+                  <a href="google.com">
+                    <img className={styles.registerImg} src={registerImg}></img>
+                  </a>
+                </>
+              )}
+            </li>
+            <hr></hr>
+            <li onClick={handleTeachClick}>
+              TEACH
+              {isClickedTeach && (
+                <>
+                  <p>
+                    Register as a <b>teacher</b> and start posting your
+                    educational offers! Join our tutors family and create your
+                    own bussiness!
+                  </p>
+                  <a href="google.com">
+                    <img className={styles.registerImg} src={registerImg}></img>
+                  </a>
+                </>
+              )}
+            </li>
+            <hr></hr>
+            <li onClick={handleAboutUsClick}>
+              ABOUT US
+              {isClickedAboutUs && (
+                <p>
+                  <b>Tutorly </b> is a web app connecting students with tutors.
+                  Students can search for tutors by subject, location, and
+                  availability. Tutors create profiles and post listings
+                  detailing their expertise, pricing, and schedules
+                </p>
+              )}
+            </li>
+            <hr></hr>
+            <li onClick={() => handleContactUsClick}>CONTACT US</li>
+          </ul>
         </div>
       </div>
     </>
