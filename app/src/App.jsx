0772a74b44e86a14ec2df70b4f6ae25727b82bb1
Oskar <hamerlao@gmail.com>
2025-01-17 23:23:06 +0100

    added route paths

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import { Home } from "./Components/Home.jsx";
 import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Login.jsx";
+import RegisterTeacher from "./Components/RegisterTeacher.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/accounts/register/teacher"
+          element={<RegisterTeacher />}
+        />
+        <Route path="/accounts/login" element={<Login />} />
       </Routes>
     </>
   );
